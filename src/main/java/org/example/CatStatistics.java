681fb3dfb7dba194dd848bab624bee79f52135a4
Constantin St <dinscalin.ks@gmail.com>
2022-10-27 00:59:50 +0700

    CatStatistic start

---
+++ src/main/java/org/example/CatStatistics.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CatStatistics {
+    public static ArrayList<Cat> sortByNameAscending(ArrayList<Cat> cats){
+       // возвращает список котов, отсортировав по имени по возрастанию.
+    }
+    public static ArrayList<Cat> sortByWeightDescending(ArrayList<Cat> cats) {
+        //возвращает список котов, отсортировав по убыванию веса.
+    }
+
+    public static ArrayList<Cat> removeFirstAndLast (ArrayList<Cat> cats) {
+        //возвращает список котов кроме первого и последнего.
+    }
+
+    public static Cat findFirstNonAngryCat (ArrayList<Cat> cats) {
+        //возвращает первого не-сердитого кота в списке.
+    }
+
+    public static int getCommonWeight(ArrayList<Cat> cats, boolean onlyAngry) {
+        //возвращает суммарный вес всех котов (если параметр onlyAngry равен true, то только сердитых котов).
+    }
+
+    public static Map<String, List<Cat>> groupCatsByFirstLetter (ArrayList<Cat> cats) {
+        //— возвращает список котов сгруппировав их по первой букве имени и отсортировав группировку по возрастанию.
+    }
+}
--- src/main/java/org/example/StackKitchen.java
+++ src/main/java/org/example/StackKitchen.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class StackKitchen<T> implements AnimalKitchen<T> {
     private int index = 0;
     private ArrayList<T> animals = new ArrayList<>();
-    
+
     @Override
     public void add(T name) {
         animals.add(name);
